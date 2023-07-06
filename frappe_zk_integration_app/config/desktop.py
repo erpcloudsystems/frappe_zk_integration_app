44d1d47366d3e68a4dfa55cede73f57eba3bd70e
Immanuel Kolapo <immanuelcaspian@gmail.com>
2023-07-06 02:34:59 +0100

    chore: black formatting

--- frappe_zk_integration_app/config/desktop.py
+++ frappe_zk_integration_app/config/desktop.py
@@ -1,12 +1,13 @@
 from frappe import _
 
+
 def get_data():
-	return [
-		{
-			"module_name": "Frappe ZK Integration App",
-			"color": "grey",
-			"icon": "octicon octicon-file-directory",
-			"type": "module",
-			"label": _("Frappe ZK Integration App")
-		}
-	]
+    return [
+        {
+            "module_name": "Frappe ZK Integration App",
+            "color": "grey",
+            "icon": "octicon octicon-file-directory",
+            "type": "module",
+            "label": _("Frappe ZK Integration App"),
+        }
+    ]
