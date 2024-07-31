a4d2dda4d6f18003803eb3e68b11c59d8b8d5bf4
Ahmed Emam <ahmedemamhatem@gmail.com>
2024-07-31 09:36:40 +0000

    Add new buttom

--- frappe_zk_integration_app/frappe_zk_integration_app/doctype/zk_device/zk_device.js
+++ frappe_zk_integration_app/frappe_zk_integration_app/doctype/zk_device/zk_device.js
@@ -10,6 +10,9 @@ frappe.ui.form.on("ZK Device", {
             frm.add_custom_button(__("Sync Employee"), function () {
                 frm.events.sync_employee(frm);
             });
+            frm.add_custom_button(__("Check Connection"), function () {
+                frm.events.check_connection(frm);
+            });
         }
     },
     get_device_logs: function (frm) {
@@ -23,7 +26,6 @@ frappe.ui.form.on("ZK Device", {
             freeze: true,
             callback: function () {
                 frappe.hide_progress();
-
                 frm.refresh();
             },
         });
@@ -39,12 +41,28 @@ frappe.ui.form.on("ZK Device", {
     },
     test_job: function (frm) {
         frappe.call({
-            method:
-                "frappe_zk_integration_app.frappe_zk_integration_app.doctype.zk_device.zk_device.get_active_device_logs",
+            method: "frappe_zk_integration_app.frappe_zk_integration_app.doctype.zk_device.zk_device.get_active_device_logs",
             callback: function () {
                 frappe.hide_progress();
                 frm.refresh();
             },
         });
     },
+    check_connection: function (frm) {
+        frappe.call({
+            method: "frappe_zk_integration_app.frappe_zk_integration_app.doctype.zk_device.zk_device.check_connection",
+            args: {
+                device_id: frm.doc.name, // Pass the device ID
+                show_progress: 1
+            },
+            freeze: true,
+            callback: function (response) {
+                if (response.message === "Success") {
+                    frappe.msgprint(__("Connection Successful"));
+                } else {
+                    frappe.msgprint(__("Connection Failed: ") + response.message);
+                }
+            }
+        });
+    }
 });
--- frappe_zk_integration_app/frappe_zk_integration_app/doctype/zk_device/zk_device.py
+++ frappe_zk_integration_app/frappe_zk_integration_app/doctype/zk_device/zk_device.py
@@ -2,16 +2,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-
 import json
 from datetime import datetime, timedelta
-
 import frappe
 from dateutil import parser
 from frappe import _
 from frappe.model.document import Document
 from frappe_zk_integration_app.zk import ZK
-
+from frappe.utils import now
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
@@ -37,18 +35,22 @@ class ZKDevice(Document):
             total = len(logs)
             if not total:
                 frappe.throw(_("Empty Logs"))
+
             if self.last_log_row:
                 self.last_log_row = parser.parse(str(self.last_log_row))
             last = self.last_log_row
+
             for log in logs:
                 if show_progress:
                     frappe.publish_progress(
                         count * 100 / total, title=_("Getting Logs...")
                     )
                 count += 1
+
                 if self.last_log_row and (log.timestamp < self.last_log_row):
                     continue
-                last_timestamp = last_log_users.get(log.user_id) or None
+
+                last_timestamp = last_log_users.get(log.user_id)
                 if period and last_timestamp:
                     diff = (log.timestamp - last_timestamp).seconds / 3600
                     if diff < period:
@@ -57,53 +59,59 @@ class ZKDevice(Document):
                 try:
                     log.status = "IN" if log.status == 1 else "OUT"
                     name = "{}-{}-{}".format(log.user_id, log.timestamp, log.status)
+
                     sql = """
-						INSERT INTO `tabDevice Log`
-							(name, employee, enroll_no, time, date, type, punch, creation, modified, owner, device)
-						VALUES
-							('{}', (SELECT name FROM tabEmployee WHERE attendance_device_id = '{}' LIMIT 1),
-							'{}', '{}', '{}', '{}', '{}', NOW(), NOW(), '{}', '{}')
-					""".format(
-                        name,
-                        log.user_id,
-                        log.user_id,
-                        log.timestamp,
+                        INSERT INTO `tabDevice Log`
+                            (name, employee, enroll_no, time, date, type, punch, creation, modified, owner, device)
+                        VALUES
+                            (%s, NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """
+                    values = (
+                        name,                
+                        log.user_id,         
+                        log.timestamp,       
                         log.timestamp.date(),
-                        log.status,
-                        log.punch,
-                        frappe.session.user,
-                        self.name,
+                        log.status,          
+                        log.punch,           
+                        frappe.utils.now(),  
+                        frappe.utils.now(),  
+                        frappe.session.user, 
+                        self.name            
                     )
 
-                    frappe.db.sql(sql)
-                    last_log_users[log.user_id] = parser.parse(str(log.timestamp))
-                except:
-                    pass
+                    # Execute SQL query
+                    frappe.db.sql(sql, values)
+                    frappe.db.commit()  
+
+                    last_log_users[log.user_id] = log.timestamp
+
+                except Exception as e:
+                    # Log error with details
+                    frappe.log_error(message=str(e), title=_("Log Insertion Error"))
+
                 last = log.timestamp
+
             if last:
                 self.last_log_row = min(last, datetime.now())
 
-            frappe.db.commit()
             conn.enable_device()
         except Exception as e:
-            frappe.msgprint(_("Process terminate : {}".format(e)), indicator="red")
+            frappe.msgprint(_("Process terminated: {}").format(e), indicator="red")
             self.last_connection_error = str(e)
         finally:
             self.last_connection_time = datetime.now()
             if conn:
                 conn.enable_device()
                 conn.disconnect()
+
         self.get_after_mins = self.get_after_mins or 5
         self.excecution_time = datetime.now() + timedelta(minutes=self.get_after_mins)
-
         self.save()
-        sync_employee()
+        self.sync_employee()
 
-
-@frappe.whitelist()
-def sync_employee():
-    try:
-        frappe.db.sql(
+    def sync_employee(self):
+        try:
+            frappe.db.sql(
                 """
                 UPDATE `tabDevice Log` log
                 SET log.employee = (
@@ -112,11 +120,29 @@ def sync_employee():
                 WHERE log.employee IS NULL OR log.employee = ''
                 """
             )
+            frappe.db.commit()
+            frappe.msgprint(_("Employee synchronization complete"))
+        except Exception as e:
+            frappe.log_error(message=str(e), title=_("Employee Synchronization Error"))
 
+
+@frappe.whitelist()
+def sync_employee():
+    try:
+        frappe.db.sql(
+            """
+            UPDATE `tabDevice Log` log
+            SET log.employee = (
+                SELECT name FROM tabEmployee WHERE attendance_device_id = log.enroll_no LIMIT 1
+            )
+            WHERE log.employee IS NULL OR log.employee = ''
+            """
+        )
         frappe.db.commit()
-        frappe.msgprint(_("Done"))
-    except:
-        pass
+        frappe.msgprint(_("Employee synchronization complete"))
+    except Exception as e:
+        frappe.log_error(message=str(e), title=_("Employee Synchronization Error"))
+
 
 @frappe.whitelist()
 def get_active_device_logs(names=None):
@@ -124,17 +150,34 @@ def get_active_device_logs(names=None):
         names = json.loads(str(names))
     cur_time = datetime.now()
     devices = names or frappe.db.sql_list(
-        f"""
-			SELECT name FROM `tabZK Device`
-			WHERE docstatus < 2 AND auto_attendance = 1
-			AND (STR_TO_DATE('{cur_time}', '%Y-%m-%d %T') >= excecution_time OR IFNULL(excecution_time, 0) = 0);
-		"""
+        """
+        SELECT name FROM `tabZK Device`
+        WHERE docstatus < 2 AND auto_attendance = 1
+        """
     )
 
     for device in devices:
         doc = frappe.get_doc("ZK Device", device)
         try:
-            doc.get_device_log()
+            doc.get_device_log(show_progress=True)
         except Exception as e:
-            frappe.msgprint(_("Process terminate : {}".format(e)), indicator="red")
+            frappe.msgprint(_("Process terminated: {}").format(e), indicator="red")
 
+
+@frappe.whitelist()
+def check_connection(device_id, show_progress=False):
+    try:
+        doc = frappe.get_doc("ZK Device", device_id)
+        zk = ZK(
+            doc.ip,
+            port=doc.port,
+            password=doc.password,
+            timeout=20,
+            force_udp=doc.udp or True,
+            ommit_ping=doc.ping or True,
+        )
+        conn = zk.connect()
+        if conn:
+            return "Success"
+    except Exception as e:
+        return str(e)
